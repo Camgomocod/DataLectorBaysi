be31056e8cb9b292b9c6bd9c3fb8a50f06009ce5
Chancar <107017249+Camgomocod@users.noreply.github.com>
2024-04-24 11:23:08 -0500

    Funcionalidad de fechas
    Se agrego correctamente el marco de las fechas, quda como pendiente obtener la variable que arroja el seleccionar la fecha y subirla a la base de datos, para así luego realizar las respectivas comprobaciones cuando el programa se vuelva a ejecutar

--- src/Interface.py
+++ src/Interface.py
@@ -1,9 +1,9 @@
+from tkinter import * 
 import tkinter as tk
-from tkinter import filedialog
 from Pdf import PdfData
+from tkcalendar import *
 import os 
 
-""" D:\General Files\Projects\DataLectorBaysi\src """
 class interface:
   def __init__(self) -> None:
     self.window = tk.Tk()
@@ -21,10 +21,24 @@ class interface:
     self.button_open_directory.grid(row=0, column=1, pady=10, padx=5)
     self.button_charge_pdf.grid(row=0, column=2, pady=10, padx=5)
     self.window_frame.grid_columnconfigure(1, weight=1)
+
+    self.selected_date = StringVar()
+
+    self.calendar = Calendar(self.window, selectmode="day", year=2024, month=4, date=24)
+    self.calendar.pack(pady=20)
+
+    self.get_date_button = Button(self.window_frame, text="Obtener fecha", command=self.get_selected_date)
+
+    self.selected_date_label = Label(self.window, textvariable=self.selected_date)
+    self.selected_date_label.pack(pady=20)
+
+    self.get_date_button.grid(row=0, column=4, pady=10, padx=5)
     
+  def get_selected_date(self):
+    self.selected_date.set(self.calendar.get_date())  
   # Crear la interfaz gráfica
   def open_directory(self):
-    folder_path = "D:\General Files\Projects\DataLectorBaysi\documents"
+    folder_path = "D:\\General Files\\Projects\\DataLectorBaysi\\documents"
     os.startfile(folder_path)
 
   def charge_pdf(self): 
@@ -32,7 +46,7 @@ class interface:
     pdfData.lectorPdfs()
   
   def open_root_directory(self):
-    folder_path = "D:\General Files\Documents\Baysi\Guias"
+    folder_path = "D:\\General Files\\Documents\\Baysi\\Guias"
     os.startfile(folder_path)
 
 
