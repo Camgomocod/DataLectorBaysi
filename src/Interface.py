3d35ecfc9168fb96c7d093af1d27b482b7d50166
Chancar <107017249+Camgomocod@users.noreply.github.com>
2024-04-25 16:12:50 -0500

    Implementacion Funcionalidades Finales
    Eliminación de algunas implementaciones anteriores y implimentación de algunas que estarán en el producto final

--- src/Interface.py
+++ src/Interface.py
@@ -1,56 +1,72 @@
 from tkinter import * 
 import tkinter as tk
+from tkinter import filedialog
 from Pdf import PdfData
 from tkcalendar import *
 import os 
+import shutil
 
 class interface:
   def __init__(self) -> None:
+  
+    # Ventana principal 
     self.window = tk.Tk()
-    self.window.geometry("400x300")
-    self.window.title("Abrir carpeta")
+    self.window.geometry("350x250")
+    self.window.config(bg="#222831")
+    self.window.title("BAYSI")
 
-    self.window_frame = tk.Frame(self.window)
-    self.window_frame.pack(side=tk.BOTTOM, pady=20)
-    
-    self.button_open_directory = tk.Button(self.window_frame, text="Abrir carpeta", command=self.open_directory)
-    self.button_open_root = tk.Button(self.window_frame, text="Carpeta Guias", command=self.open_root_directory)
-    self.button_charge_pdf = tk.Button(self.window_frame, text="CargarPdf", command=self.charge_pdf)
-    
+    self.origin_folder = tk.StringVar()
+    self.destiny_folder = tk.StringVar()
+
+    etiqueta_nombre_origen = tk.Label(self.window, text="Nombre carpeta origen:")
+    etiqueta_nombre_origen.pack()
+
+    campo_texto_nombre_origen = tk.Entry(self.window, textvariable=self.origin_folder)
+    campo_texto_nombre_origen.pack()
+
+    # Interaction Button Frame (Group buttons visually)
+    self.button_frame = tk.Frame(self.window, bg="#2C333D", bd=1, relief=tk.RAISED)
+    self.button_frame.pack(fill=tk.X, pady=20, expand=True)
+    # Declaración del apartado del calendario 
+
+    self.button_open_root = tk.Button(self.button_frame, text="Carpeta Guías", command=self.open_root_directory, padx=15)
     self.button_open_root.grid(row=0, column=0, pady=10, padx=5)
-    self.button_open_directory.grid(row=0, column=1, pady=10, padx=5)
-    self.button_charge_pdf.grid(row=0, column=2, pady=10, padx=5)
-    self.window_frame.grid_columnconfigure(1, weight=1)
-
-    self.selected_date = StringVar()
-
-    self.calendar = Calendar(self.window, selectmode="day", year=2024, month=4, date=24)
-    self.calendar.pack(pady=20)
-
-    self.get_date_button = Button(self.window_frame, text="Obtener fecha", command=self.get_selected_date)
-
-    self.selected_date_label = Label(self.window, textvariable=self.selected_date)
-    self.selected_date_label.pack(pady=20)
-
-    self.get_date_button.grid(row=0, column=4, pady=10, padx=5)
+    self.button_charge_pdf = tk.Button(self.button_frame, text="Cargar PDF", command=self.charge_pdf, padx=15)
+    self.button_charge_pdf.grid(row=0, column=1, pady=10, padx=5)
+    self.button_exit = tk.Button(self.button_frame, text="Salir", command=self.window.destroy, padx=15)
+    self.button_exit.grid(row=0, column=2, pady=10, padx=5)
     
-  def get_selected_date(self):
-    self.selected_date.set(self.calendar.get_date())  
-  # Crear la interfaz gráfica
-  def open_directory(self):
-    folder_path = "D:\\General Files\\Projects\\DataLectorBaysi\\documents"
-    os.startfile(folder_path)
-
+  
   def charge_pdf(self): 
     pdfData = PdfData()
     pdfData.lectorPdfs()
+
+  def copy_pdfs(self):
+    folder_path = "D:\\General Files\\Projects\\DataLectorBaysi\\documents"
+    if origin_path:
+      for file_name, file_type ,children_files in os.walk(origin_path):
+        # Ignorar la carpeta actual 
+        if file_name == ".":
+          continue
+
+        for file in children_files:
+          full_path = os.path.join(file_name, file)
+          # Copiar el archivo a la carpeta de destino 
+          os.makedirs(os.path.dirname(folder_path), exist_ok=True)
+          shutil.copy(full_path, folder_path)
   
   def open_root_directory(self):
+    global origin_path
     folder_path = "D:\\General Files\\Documents\\Baysi\\Guias"
-    os.startfile(folder_path)
-
+    origin_path = filedialog.askdirectory(initialdir=folder_path, title="Seleccione la carpeta de origen")
+    if origin_path:
+      self.origin_folder.set(os.path.basename(origin_path))
+      self.copy_pdfs()
+  
 
 if __name__ == "__main__":
   interfaceG = interface()
   interfaceG.window.mainloop()
+  folder_name = interfaceG.origin_folder.get()
+  print(f"El nombre de la carpeta es {folder_name}")
   
